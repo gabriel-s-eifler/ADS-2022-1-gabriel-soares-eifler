32c719dcb974d55228c157d12645bb8635f73004
gabriel-s-eifler <101073024+gabriel-s-eifler@users.noreply.github.com>
2022-03-28 17:14:22 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+int codigo,qtd;
+float productv, valornota , discount, valorfinalnota;
+
+printf("código do produto?\n");
+
+scanf ("%d",&codigo);
+
+printf ("quantidade comprada?\n");
+
+scanf ("%d",&qtd);
+
+if (codigo >=1 && codigo <=10){
+   productv=10;
+
+}
+  if (codigo>=11 && codigo<=20) {
+    productv=15;
+
+}
+
+if (codigo>=21 && codigo<=30) {
+      productv=20;
+}
+
+if (codigo>=3 && codigo<=40){
+    productv=30;
+}
+
+ valornota = productv * qtd;
+printf ("Valor da nota sem desconto:\n",valornota);
+
+if (valornota<250){
+    discount=valornota*0.05;
+
+        if (valornota<=500)
+       discount=valornota * 0.1;
+
+
+else
+       discount=valornota * 0.15;
+
+}
+
+ valorfinalnota = valornota - discount;
+printf ("Valor do desconto é:\n",discount);
+printf ("Valor da nota é: \n",valorfinalnota);
+}
+
+
+
