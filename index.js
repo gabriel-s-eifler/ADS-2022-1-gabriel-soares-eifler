ffea054ea894be0653bfddee2f06df1fedfb59c4
gabriel-s-eifler <101073024+gabriel-s-eifler@users.noreply.github.com>
2022-04-20 22:14:31 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const nume = parseInt(prompt('digite um numero inteiro para ser multiplicado:  '));
-
-for(let um = 1; um <= 10; um++) {
-
-    const result = um * nume;
-     console.log(prompt(result));
-}
